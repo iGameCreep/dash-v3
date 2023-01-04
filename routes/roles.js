226e0afe71b9e82133a5f0a0b6075d0139d9b79d
iGameCreep <gamecreepbs@gmail.com>
2023-01-04 20:28:01 +0100

    Fixed error with role.js route file.

--- routes/roles.js
+++ routes/roles.js
@@ -16,35 +16,33 @@ router.get('/guild/:gid/roles', ensureAuthenticated,(req, res) => {
     let gid = req.params.gid 
     var theme = jsonfile.readFileSync(themes);
     let fetchguilds = discord.client.guilds.fetch({ cache: true, withCounts: true })
-
-    let rauth = guild.members.fetch(profile.id).catch(console.log)
-
-    rauth.then((rauth) => {
-        if (!rauth) {
-            req.flash('error', 'You are not in this guild !')
-            return res.redirect('/dash')
-        }
-    
-        if (!rauth.permissions.has(Permissions.FLAGS.MANAGE_GUILD) && !rauth.permissions.has(Permissions.FLAGS.ADMINISTRATOR && rauth.id !== guild.ownerId)) {
-            req.flash('error', 'You are allowed to manage this guild !')
-            return res.redirect('/dash')
-        }
     
         fetchguilds.then(gs => {
             let guild = discord.client.guilds.cache.get(gid)
             let roles = guild.roles.cache
-    
-            res.render('home/roles', {
-                profile:req.user,
-                client:discord.client,
-                theme:theme,
-                all:req,
-                config: config,
-                guild: guild,
-                roles: roles
-            }) 
-        })  
-    }) 
+            let rauth = guild.members.fetch(req.user.id).catch(console.log)
+
+            rauth.then((rauth) => {
+                if (!rauth) {
+                    req.flash('error', 'You are not in this guild !')
+                    return res.redirect('/dash')
+                }
+            
+                if (!rauth.permissions.has(Permissions.FLAGS.MANAGE_GUILD) && !rauth.permissions.has(Permissions.FLAGS.ADMINISTRATOR && rauth.id !== guild.ownerId)) {
+                    req.flash('error', 'You are allowed to manage this guild !')
+                    return res.redirect('/dash')
+                }
+                res.render('home/roles', {
+                    profile:req.user,
+                    client:discord.client,
+                    theme:theme,
+                    all:req,
+                    config: config,
+                    guild: guild,
+                    roles: roles
+                }) 
+            })  
+        }) 
 })
   
 module.exports = router;
